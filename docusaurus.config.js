79d8cf38e9110dc207873527d634be92aaba6e9c
Joel Hans <j@joelhans.com>
2022-09-21 10:45:04 -0700

    Initialize new docs homepage

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -58,6 +58,16 @@ const config = {
         },
         items: [
           {
+            href: '/what-is-radicle',
+            label: 'User Hub',
+            position: 'left',
+          },
+          {
+            href: '/community',
+            label: 'Community Hub',
+            position: 'left',
+          },
+          {
             href: 'https://radicle.xyz',
             label: 'Radicle',
             position: 'right',
